c98e131f8ebe05e02b464aea44d4f932685392ef
Waseem Tesqie <waseemtesqie@gmail.com>
2020-01-20 11:54:11 -0500

    Update ssss.py

--- test/ssss.py
+++ test/ssss.py
@@ -1,26 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-pydot example 1
-@author: Federico Cáceres
-@url: http://pythonhaven.wordpress.com/2009/12/09/generating_graphs_with_pydot
-"""
-import pydot # import pydot or you're not going to get anywhere my friend :D
+import pydot
 
-# first you create a new graph, you do that with pydot.Dot()
 graph = pydot.Dot(graph_type='graph')
 
-# the idea here is not to cover how to represent the hierarchical data
-# but rather how to graph it, so I'm not going to work on some fancy
-# recursive function to traverse a multidimensional array...
-# I'm going to hardcode stuff... sorry if that offends you
-
-# let's add the relationship between the king and vassals
 for i in range(3):
-    # we can get right into action by "drawing" edges between the nodes in our graph
-    # we do not need to CREATE nodes, but if you want to give them some custom style
-    # then I would recomend you to do so... let's cover that later
     # the pydot.Edge() constructor receives two parameters, a source node and a destination
-    # node, they are just strings like you can see
     edge = pydot.Edge("king", "lord%d" % i)
     # and we obviosuly need to add the edge to our graph
     graph.add_edge(edge)
