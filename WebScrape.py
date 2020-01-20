1e11db8814af652955f219fec8a8e492cb1a1c29
Waseem Tesqie <waseemtesqie@gmail.com>
2020-01-20 10:39:22 -0500

    Add files via upload

---
+++ WebScrape.py
@@ -0,0 +1,35 @@
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1',
+                       user='root', passwd='5565', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute('USE wikipedia')
+
+def getUrl(pageId):
+    cur.execute('SELECT url FROM pages WHERE id = %s', (int(pageId)))
+    return cur.fetchone()[0]
+
+def getLinks(fromPageId):
+    cur.execute('SELECT toPageId FROM links WHERE fromPageId = %s',
+                (int(fromPageId)))
+    if cur.rowcount == 0:
+        return []
+    return [x[0] for x in cur.fetchall()]
+
+def searchBreadth(targetPageId, paths=[[1]]):
+    newPaths = []
+    for path in paths:
+        links = getLinks(path[-1])
+        for link in links:
+            if link == targetPageId:
+                return path + [link]
+            else:
+                newPaths.append(path+[link])
+    return searchBreadth(targetPageId, newPaths)
+
+nodes = getLinks(1)
+print(nodes)
+targetPageId = 28624
+pageIds = searchBreadth(targetPageId)
+for pageId in pageIds:
+    print(getUrl(pageId))
